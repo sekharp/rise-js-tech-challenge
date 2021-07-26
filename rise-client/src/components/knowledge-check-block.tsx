bae4af67bb2711202ef4918bac7a523f64f1769a
Sekhar Paladugu <sekharp@gmail.com>
2021-07-26 09:43:17 -0600

    render options buttons, tsconfig tweak

--- rise-client/src/components/knowledge-check-block.tsx
+++ rise-client/src/components/knowledge-check-block.tsx
@@ -2,14 +2,14 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const KnowledgeCheckBlock = () => {
-  const [knowledgeCheckData, setKnowledgeCheckData] = useState([]);
+  const [knowledgeCheckData, setKnowledgeCheckData] = useState({});
 
   useEffect(() => {
     axios
       .get(`http://localhost:5000/knowledge-check-blocks`)
       .then((res) => res.data)
       .then((res) => {
-        setKnowledgeCheckData(res)
+        setKnowledgeCheckData(res[0] as any)
       })
       .catch(console.error);
   }, []);
@@ -20,7 +20,16 @@ const KnowledgeCheckBlock = () => {
     <>
       <h3>Knowledge Check Block</h3>
       <div className='knowledge-check-block'>
-        {JSON.stringify(knowledgeCheckData)}
+        <p>What is this a picture of?</p>
+        <img className="knowledge-check-image" alt="knowledge-check-img" src={(knowledgeCheckData as any)?.question?.media?.url} />
+        {(knowledgeCheckData as any)?.answers?.map((a, i) => {
+          return (<div className={`option ${a?.text}`} key={i}>
+            <label>
+              <input type="radio" checked={a?.text} />
+              {a.text}
+            </label>
+          </div>)
+        })}
       </div>
     </>
   );
